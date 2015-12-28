69957afb9a72bd40308edd3f6aad2bda3da0dde1
unknown <sebner@AUCLPSLE188.us.planview.world>
2015-12-28 11:47:30 -0600

    Added link to the login (home) page.

--- app/views/master/errors.blade.php
+++ app/views/master/errors.blade.php
@@ -9,6 +9,12 @@
         <h1>The Page You Requested Cannot Be Found</h1>
     </header>
     <article>
-        <p>We're sorry. You have come across a page which does not exist or has been moved.</p>
+    	<?php
+		if (Auth::check()) {
+			echo '<p>We&rsquo;re sorry. You have come across a page which does not exist or has been moved.</p>';
+		} else {
+			echo '<p>We&rsquo;re sorry. You have come across a page which does not exist or has been moved, or you may need to <a href="/" title="Login">login</a> to view the page.</p>';
+		}
+		?>
     </article>
 @stop
